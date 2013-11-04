4c1104fdb9799eac842b8c549b3aebad97ea45be
Adnan Begovic <adnan@cyngn.com>
2013-11-04 00:58:57 +0000

    frameworks: Add unlinked ringtone and notification volumes
    Port over linked volumes from 12.1
    
    Implement linked volumes and add upgrade path.
    
    Change-Id: Ibbeff656fcaaa4a2ff79de530b48b216df271b21
    
    frameworks: read VOLUME_LINK_NOTIFICATION from proper table
    
    Change-Id: I15e9caa18f307a51a732c7a59013bbbb234a7477
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 1b7cfa1716017e0d03c9693428c21d8e11c5dc0a)
    
    fix Notification volume slider linking
    
    Change-Id: I0f74485f5c8c2aa7698cf9ba1c60c2c8c967798d
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Improve SeekBarVolumizer behavior with unlinked streams
    
    - Fix notification stream enabling at 0 volume
    - Fix the notification slider being set when moving ringer stream
    
    Change-Id: If3705cc833c07f726f8ad7b1ec79a42e86e8ff22
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    base: Fix notification sounds for wifi only devices
    
    The linking of voice & notification doesn't take into account the
    fact that a device may not actually have voice capabilities.  Whenever
    checking whether or not to link notification and ring volumes, also
    verify that the device really is voice capable.
    
    Change-Id: Iae49302d3b5934d36b182e289e4975f5dd85b834
    
    frameworks: fix setting up linked notification stream on boot
    
    updateStreamVolumeAlias() was previously responsible for checking the
    linked notification variable and mapping the notification stream, but
    the setting to read whether the notifications were linked was read in
    after that method had run which was responsible for linking them.
    Extract the logic and re-run it again after reading the persisted
    settings.
    
    Change-Id: Ia62bf3ca072def54ec6ebf4f9e865d9c913e3bc4
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    fixup volume stream linking
    
    - Make the SeekBarVolumizer move notification and ringer streams when
     linked and moving ringer.
    
    - Fix initial state causing volume stream seek bar to be set to 0
      initially
    
    - Clean up AudioService initialization code
    
    Change-Id: Ia11a7a392e5eb3ef862147843e365d5f20eaf90a
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: Fix volume panel inconsistencies
    
    * Use xml drawables everywhere
    * Add muted notification icon
    * Use AOSP naming scheme for drawables
    Use STREAM_RING on non-voice capable devices
    
    * That's what Google does.
    
    Change-Id: I5bc77ec516b91d5a7ba9b93bc7a0374dc425c915

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2567,6 +2567,8 @@ public final class Settings {
 
             // At one time in System, then Global, but now back in Secure
             MOVED_TO_SECURE.add(Secure.INSTALL_NON_MARKET_APPS);
+            MOVED_TO_SECURE.add(Secure.VOLUME_LINK_NOTIFICATION);
+
         }
 
         @UnsupportedAppUsage
@@ -9221,6 +9223,14 @@ public final class Settings {
         private static final Validator AWARE_LOCK_ENABLED_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        private static final Validator VOLUME_LINK_NOTIFICATION_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
          * The TCP/IP port to run ADB on, or -1 for USB
          * @hide
          */
@@ -9471,6 +9481,7 @@ public final class Settings {
             STATUSBAR_CLOCK_DATE_DISPLAY,
             STATUSBAR_CLOCK_DATE_STYLE,
             STATUSBAR_CLOCK_DATE_FORMAT,
+            VOLUME_LINK_NOTIFICATION,
         };
 
         /**
@@ -9678,6 +9689,7 @@ public final class Settings {
             VALIDATORS.put(STATUSBAR_CLOCK_POSITION, STATUSBAR_CLOCK_POSITION_VALIDATOR);
             VALIDATORS.put(DOZE_ON_CHARGE_NOW, DOZE_ON_CHARGE_NOW_VALIDATOR);
             VALIDATORS.put(DOZE_ON_CHARGE, DOZE_ON_CHARGE_VALIDATOR);
+            VALIDATORS.put(VOLUME_LINK_NOTIFICATION, VOLUME_LINK_NOTIFICATION_VALIDATOR);
         }
 
         /**
