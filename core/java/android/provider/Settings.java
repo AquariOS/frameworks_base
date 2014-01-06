273f686b6c33418426cb908d683b0a73fd55425e
James Owers <Jmz@users.noreply.github.com>
2014-01-06 15:30:15 -0500

    Force Expanded Notifications [1/2]
    This will force expanded notifications for all apps that support expanded notifications
    
    Change-Id: Ie7bfdaa778f6e101dca1520d531d7a1875d300f4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4835,6 +4835,12 @@ public final class Settings {
         public static final String BATTERY_FULLY_CHARGED_NOTIF = "battery_fully_charged_notif";
 
         /**
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
