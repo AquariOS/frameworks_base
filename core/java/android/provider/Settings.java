bf38c5c4a2a4da9e331d3666685d27ff198d9a09
cphelps76 <cphelps76@droidconcepts.com>
2014-03-19 16:23:35 +0000

    base: Add back battery bar [1/2]
    {x} Adds Battery bar with following features:
    {x} aligment
    {x} thickness
    {x} charging animation
    
    @mydongistiny: updated for oreo
    Change-Id: Ie9a168121dc569ec3af068a4bda6b46a4e896b2d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4007,59 +4007,6 @@ public final class Settings {
         public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
 
         /**
-<<<<<<< HEAD
-         * Whether to show the battery bar
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_CHARGING_COLOR = "statusbar_battery_bar_charging_color";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_BATTERY_LOW_COLOR = "statusbar_battery_bar_battery_low_color";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
-
-        /**
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_ENABLE_CHARGING_COLOR = "battery_bar_enable_charging_color";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_BLEND_COLORS = "battery_bar_blend_color";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_BLEND_COLORS_REVERSE = "battery_bar_blend_color_reverse";
-
-        /**
          * Change volume up and down handlign based on rotation
          * @hide
          */
@@ -4333,10 +4280,33 @@ public final class Settings {
                                 "show_lockscreen_visualizer";
         public static final int SHOW_LOCKSCREEN_VISUALIZER_DEFAULT = 1;
 
+        /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String BATTERY_BAR_LOCATION = "battery_bar_location";
 
         /**
-=======
->>>>>>> parent of 4f88fa9... [SQUASH] Battery Bar with DUI support and color blending [1/2]
+         * @hide
+         */
+        public static final String BATTERY_BAR_COLOR = "battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_THICKNESS = "battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_STYLE = "battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_ANIMATE = "battery_bar_animate";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
