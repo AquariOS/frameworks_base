19718826739bbb6351b652e8e8608a07849ca186
Roman Birg <roman@cyngn.com>
2014-11-09 19:52:34 -0800

    Add QS pull down with one finger [1/2]
    Adjust DoubleTapToSleep code in order to keep calls separate
    
    Change-Id: I7aab18b75b339e5d6d2215f7259d50314476e0f7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4848,6 +4848,12 @@ public final class Settings {
         public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
 
         /**
+         * Allow users to pull down the status bar quickly
+         * @hide
+         */
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
