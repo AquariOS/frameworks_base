21599b52653922fe191dc7546cbdaf1a402ae4c1
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-11 20:07:15 +0000

    QuickSettings: Option to disable brightness slider (1/2)
    Change-Id: I98a28e99e78ee4d8bb4f03bc53e867723d62b052
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8270,6 +8270,12 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /**
+         * Whether to show the brightness slider in quick settings panel
+         * @hide
+         */
+        public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
