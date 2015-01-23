94c33591f01cecef60ebb11eb5bf559b57929587
Danesh M <daneshm90@gmail.com>
2015-01-23 11:57:52 -0800

    QS: SystemUI: ADB over network tile
    mydongistiny: Fixed up for Pie by using the secondary label
    for host address and using only one drawable
    
    Change-Id: I30256bdec4379b870129070e1a9adc93ee7d4043

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -23,6 +23,7 @@ import com.android.systemui.R;
 import com.android.systemui.plugins.qs.*;
 import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.external.CustomTile;
+import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -113,6 +114,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SmartNavTile(mHost);
             case "expanded_desktop":
                 return new ExpandedDesktopTile(mHost);
+            case "adb_network":
+                return new AdbOverNetworkTile(mHost);
         }
 
         // Intent tiles.
