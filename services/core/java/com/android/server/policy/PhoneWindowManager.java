c4607c33c71413f2687e77cc42ea11681c3ec3cb
Christopher R. Palmer <crpalmer@gmail.com>
2015-05-01 06:15:47 -0400

    Fix volume key music control with ambient display enabled
    Previously the dream service would only report whether or not it was dreaming
    
    Ambient display counts as dreaming.  When dreaming, the volume inputs are
    passed back to the application which is not what we want for ambient
    display
    
    Fix this by exposing isDozing() and allow volume keys to control music
    when we are dreaming and have moved into doze mode (aka, sleeping)
    
    Change-Id: I3b1e72b95ec8b1b63ef0219259dfee77a5d339d3

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6547,7 +6547,7 @@ g            case KeyEvent.KEYCODE_ENDCALL: {
         IDreamManager dreamManager = getDreamManager();
 
         try {
-            if (dreamManager != null && dreamManager.isDreaming()) {
+            if (dreamManager != null && dreamManager.isDreaming() && !dreamManager.isDozing()) {
                 return true;
             }
         } catch (RemoteException e) {
