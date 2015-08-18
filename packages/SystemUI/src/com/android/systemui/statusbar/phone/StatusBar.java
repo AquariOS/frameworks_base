35837d95f2912a8d09b57e6839cb6f866370e3ec
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2015-08-18 12:27:39 +0530

    SystemUI: Add visualizer feature
    This is a squash of the following commits:
    
    Author: Utkarsh Gupta <utkarsh.eminem@gmail.com>
    Date:   Tue Aug 18 12:27:39 2015 +0530
    
        SystemUI: use new visualizer from Eleven
    
        Change-Id: I4441440868cac71caa32c91a0875f00a630fe596
        Signed-off-by: Utkarsh Gupta <utkarsh.eminem@gmail.com>
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Mikalacki Sava <mikalackis@gmail.com>
    Date:   Thu Sep 3 23:10:35 2015 +0200
    
        SystemUI: fix NPE crash when Visualizer is not initialized
    
        Change-Id: Ic3774011acfae0312f1642d88c7f5bf2abc9f5a7
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Fri Sep 4 15:55:39 2015 -0700
    
        SystemUI: hide visualizer when keyguard is occluded
    
        Change-Id: Ia943fd829f7a379fee0f657bb08c34b8c6f36caa
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Jan 7 19:27:38 2016 +0100
    
        SystemUI: Make lockscreen visualizer battery friendly
    
        In case of offloaded playback, the device is allowed to sleep.
        However, if the lockscreen visualizer is enabled, the device
        hardly enters deep sleep, even if the screen is off. To prevent
        this, enable the visualizer only when the lockscreen is shown.
    
        Change-Id: I484270694b734c9b53e61a7c0ad74391b54fe8cd
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Mar 17 09:18:56 2016 -0700
    
        SystemUI: more consistent visualizer logic
    
        - only call setVisible from screen on and screen off methods
        - eagerly unregister self to make sure we don't get added twice as a
          listener
        - Add some debugging under the DEBUG flag
    
        Change-Id: Iada13058f87d4c8d8c8b60f6eebf055652ff2c8d
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Mon Apr 4 15:29:34 2016 -0700
    
        SystemUI: show visualizer in shade-locked view
    
        This brings back the behavior we had in 12.1 - visualizer would still be
        visible in the shade locked state.
    
        Also with the following improvements:
            * use less alpha
            * always attach/detach in a background thread (Async task)
            * use the statusbar state instead keyguard view state listener
    
        Change-Id: I6b8a57f9f0bba6ba0591de0cb1f94a0eb904cc1c
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Dan Pasanen <invisiblek@cyanogenmod.org>
    Date:   Wed May 18 20:27:47 2016 -0500
    
        Visualizer: Let visualizer fill the entire available screen
    
        * This will allow it to be more dynamic (landscape lock screen for instance)
    
        Change-Id: I2ffd6b0ca8832812faaa4cc0ec4cdd6071fbec8c
    
    Author: Matthias Yzusqui <myzb.dev@gmail.com>
    Date:   Wed Sep 21 15:25:00 2016 +0200
    
        SystemUI: fix navbar drawing glitch
    
        Change-Id: I58e255517d35675c043061f2edb398dab0d16ba8
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Oct 7 08:56:25 2016 +0800
    
        SystemUI: Use Tuner API for CM settings
    
        Get rid of all the excess code by implementing TunerService.Tunable
        and observing any changes made to the settings through it.
    
        Remove UserContentObserver as the Tuner API handles user switches.
    
        Also remove some unused imports that were introduced in earlier
        CM commits, but were never removed since.
    
        Change-Id: Iecafafabdaec82b3b3c72293bea865de48f0e90a
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Dec 2 15:10:28 2017 +0000
    
        SystemUI: Forward-port visualizer feature to O
    
    Change-Id: I7a8497eaac9129f14b328e070c936e135fa85db6
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -235,6 +235,7 @@ import com.android.systemui.statusbar.ScrimView;
 import com.android.systemui.statusbar.SignalClusterView;
 import com.android.systemui.statusbar.StatusBarState;
 import com.android.systemui.statusbar.notification.AboveShelfObserver;
+import com.android.systemui.statusbar.VisualizerView;
 import com.android.systemui.statusbar.notification.InflationException;
 import com.android.systemui.statusbar.notification.RowInflaterTask;
 import com.android.systemui.statusbar.notification.VisualStabilityManager;
@@ -638,6 +639,11 @@ public class StatusBar extends SystemUI implements DemoMode,
     private Entry mEntryToRefresh;
     private NotificationManager mNoMan;
     private String[] mNavMediaArrowsExcludeList;
+
+    private VisualizerView mVisualizerView;
+    private boolean mScreenOn;
+    private boolean mKeyguardShowingMedia;
+
     private MediaSessionManager mMediaSessionManager;
     private MediaController mMediaController;
     private String mMediaNotificationKey;
@@ -654,6 +660,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     updateMediaMetaData(true, true);
                 }
                 setMediaPlaying();
+                mVisualizerView.setPlaying(state.getState() == PlaybackState.STATE_PLAYING);
             }
         }
 
@@ -1304,6 +1311,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mHeadsUpManager.addListener(mScrimController);
         mStackScroller.setScrimController(mScrimController);
         mDozeScrimController = new DozeScrimController(mScrimController, context);
+        mVisualizerView = (VisualizerView) mStatusBarWindow.findViewById(R.id.visualizerview);
 
         // Other icons
         mVolumeComponent = getComponent(VolumeComponent.class);
@@ -1374,9 +1382,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
 
         PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-        if (!pm.isScreenOn()) {
-            mBroadcastReceiver.onReceive(mContext, new Intent(Intent.ACTION_SCREEN_OFF));
-        }
+        mBroadcastReceiver.onReceive(mContext,
+                new Intent(pm.isScreenOn() ? Intent.ACTION_SCREEN_ON : Intent.ACTION_SCREEN_OFF));
         mGestureWakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK,
                 "GestureWakeLock");
         mVibrator = mContext.getSystemService(Vibrator.class);
@@ -1391,6 +1398,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_ON);
         filter.addAction(DevicePolicyManager.ACTION_SHOW_DEVICE_MONITORING_DIALOG);
         filter.addAction(AquaUtils.ACTION_DISMISS_KEYGUARD);
         context.registerReceiverAsUser(mBroadcastReceiver, UserHandle.ALL, filter, null, null);
@@ -2665,6 +2673,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                 artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), artworkBitmap);
             }
         }
+        mKeyguardShowingMedia = artworkDrawable != null;
+
         boolean allowWhenShade = false;
         if (ENABLE_LOCKSCREEN_WALLPAPER && artworkDrawable == null) {
             Bitmap lockWallpaper = mLockscreenWallpaper.getBitmap();
@@ -2681,7 +2691,22 @@ public class StatusBar extends SystemUI implements DemoMode,
         boolean hideBecauseOccluded = mStatusBarKeyguardViewManager != null
                 && mStatusBarKeyguardViewManager.isOccluded();
 
+        final boolean keyguardVisible = (mState != StatusBarState.SHADE);
         final boolean hasArtwork = artworkDrawable != null;
+
+        if (!mKeyguardFadingAway && keyguardVisible && hasArtwork && mScreenOn) {
+            // if there's album art, ensure visualizer is visible
+            mVisualizerView.setPlaying(mMediaController != null
+                    && mMediaController.getPlaybackState() != null
+                    && mMediaController.getPlaybackState().getState()
+                            == PlaybackState.STATE_PLAYING);
+        }
+
+        if (keyguardVisible && mKeyguardShowingMedia &&
+                (artworkDrawable instanceof BitmapDrawable)) {
+            // always use current backdrop to color eq
+            mVisualizerView.setBitmap(((BitmapDrawable)artworkDrawable).getBitmap());
+        }
 
         if ((hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
                 && (mState != StatusBarState.SHADE || allowWhenShade)
@@ -4246,8 +4271,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                 }
             }
             else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+                mScreenOn = false;
                 finishBarAnimations();
                 resetUserExpandedStates();
+            }
+            else if (Intent.ACTION_SCREEN_ON.equals(action)) {
+                mScreenOn = true;
             }
             else if (DevicePolicyManager.ACTION_SHOW_DEVICE_MONITORING_DIALOG.equals(action)) {
                 mQSPanel.showDeviceMonitoringDialog();
@@ -5460,6 +5489,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mNotificationPanel.setDark(mDozing, animate);
         updateQsExpansionEnabled();
         mDozeScrimController.setDozing(mDozing, animate);
+        mVisualizerView.setDozing(mDozing);
         updateRowStates();
         Trace.endSection();
     }
@@ -5595,6 +5625,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             maybeEscalateHeadsUp();
         }
         mState = state;
+        mVisualizerView.setStatusBarState(state);
         mGroupManager.setStatusBarState(state);
         mHeadsUpManager.setStatusBarState(state);
         mFalsingManager.setStatusBarState(state);
@@ -6051,11 +6082,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         public void onScreenTurnedOn() {
             mScrimController.wakeUpFromAod();
             mDozeScrimController.onScreenTurnedOn();
+            mVisualizerView.setVisible(true);
         }
 
         @Override
         public void onScreenTurnedOff() {
             mFalsingManager.onScreenOff();
+            mVisualizerView.setVisible(false);
             // If we pulse in from AOD, we turn the screen off first. However, updatingIsKeyguard
             // in that case destroys the HeadsUpManager state, so don't do it in that case.
             if (!isPulsing()) {
@@ -6219,6 +6252,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         updateDozingState();
         Trace.endSection();
+    }
+
+    public VisualizerView getVisualizer() {
+        return mVisualizerView;
     }
 
     public boolean isKeyguardShowing() {
