23bb23003fce8c68574c327f8425c84f95102895
beanstown106 <nbenis106@gmail.com>
2015-10-23 21:07:35 -0400

    Disable Lockscreen Media Art [1/2]
    abc ezio84: move the setting check to the custom observer
    disable art on lockscreen by default
    
    * toggle to choose whether or not to show album art/media art on lockscreen
    
    Special thanks to @atl4tis from Cataclysm rom for pointing out where to hook into this

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4398,6 +4398,12 @@ public final class Settings {
         public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
 
         /**
+        * Whether to show media art on lockscreen
+        * @hide
+        */
+       public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4574,6 +4580,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER_IMAGE);
             // Pocket mode handler.
             PRIVATE_SETTINGS.add(POCKET_JUDGE);
+            PRIVATE_SETTINGS.add(LOCKSCREEN_MEDIA_METADATA);
         }
 
         /**
