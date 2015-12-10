8ea5928a7e4d64d8132a8787255d9d6d4ef20261
beanstown106 <nbenis106@gmail.com>
2015-12-10 15:51:00 -0500

    Base: longpress settings button for Flash Settings

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
@@ -144,6 +144,7 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
         mSettingsButton = (SettingsButton) findViewById(R.id.settings_button);
         mSettingsContainer = findViewById(R.id.settings_button_container);
         mSettingsButton.setOnClickListener(this);
+        mSettingsButton.setOnLongClickListener(this);
 
         mAlarmStatusCollapsed = findViewById(R.id.alarm_status_collapsed);
         mAlarmStatus = (TextView) findViewById(R.id.alarm_status);
@@ -389,6 +390,8 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
             startDateLongClickActivity();
         } else if (v == mMultiUserSwitch) {
             startUserLongClickActivity();
+        } else if (v == mSettingsButton) {
+            startSettingsLongClickActivity();
         }
         return false;
     }
@@ -396,6 +399,13 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
     private void startSettingsActivity() {
         mActivityStarter.startActivity(new Intent(android.provider.Settings.ACTION_SETTINGS),
                 true /* dismissShade */);
+    }
+
+    private void startSettingsLongClickActivity() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+	intent.setClassName("com.android.settings",
+            "com.android.settings.Settings$FlashSettingsActivity");
+        mActivityStarter.startActivity(intent, true /* dismissShade */);
     }
 
     private void startClockActivity() {
