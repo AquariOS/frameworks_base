83a9a0c17e3bc54e76c1d20a86583009f3650ca2
BeansTown106 <nbenis106@gmail.com>
2016-06-12 00:16:58 -0400

    Lockscreen Item Options [1/2] *Clock toggle *Date toggle *Alarm Text toggle
    The previous lockscreen mods were a mess, rewrote them also added an observer to reduce the io as the old mods were "updating" every refresh of the clock

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3908,6 +3908,24 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
         /**
+         * show lockscreen alarm
+         *  @hide
+         */
+        public static final String SHOW_LOCKSCREEN_ALARM = "show_lockscreen_alarm";
+
+        /**
+         * show lockscreen clock
+         *  @hide
+         */
+        public static final String SHOW_LOCKSCREEN_CLOCK = "show_lockscreen_clock";
+
+        /**
+         * show lockscreen date
+         *  @hide
+         */
+        public static final String SHOW_LOCKSCREEN_DATE = "show_lockscreen_date";
+
+        /**
          * Whether to control brightness from status bar
          * @hide
          */
