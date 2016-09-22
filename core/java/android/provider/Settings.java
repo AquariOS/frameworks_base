2985ef7c1d455548f1578a8250bc59ed9f09f9d4
Danesh M <danesh@cyngn.com>
2016-09-22 02:59:05 -0400

    SystemUI : Port forward statusbar brightness control Brings back the status bar brightness control from cm-10.2/cm-11
    SystemUI: improve brightness slider behavior
    Mimic the brightness slider behavior in the statusbar.
    This adds logic to make the statusbar slider also work with automatic
    brightness mode enabled and it will instead adjust the temporary automatic
    brightness overrride.
    Change-Id: I78814a785decc83df94844fcc2f489c8cbec2b65
    
    SystemUI: status bar brightness: store value as int
    Non-automatic brightness value is stored as int, not float.
    Symptom: adjust the brightness in the status bar, the brightness
             slider in the notification bar will always be set to full
    Change-Id: Icc04e7da3286e9925f8b90528c1e022b33d33e9d
    
    SystemUI: fix status bar brightness slider eating events
    In some cases it would eat the event and not lose focus to the rest of
    the system.
    Change-Id: I94d532d09ea762edf7ac037043ed023caf42c401
    
    SystemUI : fix brightness slider for automatic brightness
    BRIGHTNESS_ADJ_RESOLUTION was changed to 2048 from 100.
    Update the calculation to reflect this change.
    Change-Id: I560109e555e3077c7247856e63b017007098ba1f
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3717,6 +3717,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Whether to control brightness from status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
