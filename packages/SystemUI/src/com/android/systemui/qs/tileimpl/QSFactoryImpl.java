c8447356048d534dfb94f0c6cf54331cce064ef5
Danesh M <daneshm90@gmail.com>
2016-09-26 03:53:28 +0200

    Adb over Network: add QS tile [2/3]
    Also included the following from Omni - which have already been added to source (to avoid repetative commits with each new tile):
    - "base: SystemUI: stop crashing qs tiles during boot"
    - "SystemUI: use slashify for more qs tile" by @maxwen
    
    abc ezio84:
    show ip address and port in the notification too
    adapt to O
    fix disabled icon color
    improve strings
    
    Change-Id: I553ed1c4b7237c5ecac8b69ab7a94259d8de1ceb
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -22,6 +22,7 @@ import com.android.systemui.R;
 import com.android.systemui.plugins.qs.*;
 import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.external.CustomTile;
+import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -93,6 +94,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(mHost);
         else if (tileSpec.equals("smartpixels")) return new SmartPixelsTile(mHost);
         else if (tileSpec.equals("soundsearch")) return new SoundSearchTIle(mHost);
+        else if (tileSpec.equals("adb_network")) return new AdbOverNetworkTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
