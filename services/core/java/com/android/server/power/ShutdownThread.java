d3bf217abf0b86d2af08f76b2beee4a0a6b865e7
LorDClockaN <lordclockan@gmail.com>
2016-09-28 20:01:58 +0200

    Add "Restart SystemUI" to power menu using @Surge1223's rootless method
    Surge's rootless method: https://github.com/VelvetProject/packages_apps_ThemeInterfacer/commit/3484db3dda1e7bf7e3f6fda049b903968f1506e7)
    
    This is essentially a squash of @SpiritCroc's re-work of the following commits:
    
    Add Restart SystemUI to reboot menu: http://gerrit.aicp-rom.com/#/c/28764/
    Rewrite restart SystemUI method: http://gerrit.aicp-rom.com/#/c/38696/
    
    AquariOS edit: removed Slim shortcut option
    
    Change-Id: Ie00ffd52fec29e47ab8613d6feaf065542b40bd3

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -65,6 +65,8 @@ import java.io.IOException;
 
 import com.android.internal.R;
 
+import com.android.internal.util.aquarios.Helpers;
+
 public final class ShutdownThread extends Thread {
     // constants
     private static final String TAG = "ShutdownThread";
@@ -81,6 +83,7 @@ public final class ShutdownThread extends Thread {
     private static final int RADIO_STOP_PERCENT = 18;
     private static final int MOUNT_SERVICE_STOP_PERCENT = 20;
     private static final String SOFT_REBOOT = "soft_reboot";
+    private static final String SYSTEMUI_REBOOT = "systemui_reboot";
 
     // length of vibration before shutting down
     private static final int SHUTDOWN_VIBRATE_MS = 500;
@@ -221,7 +224,11 @@ public final class ShutdownThread extends Thread {
                                 if (selected != ListView.INVALID_POSITION) {
                                     String actions[] = context.getResources().getStringArray(
                                             com.android.internal.R.array.shutdown_reboot_actions);
-                                    if (selected >= 0 && selected < actions.length) {
+                                    if (actions[selected].equals(SYSTEMUI_REBOOT)) {
+                                        mReason = actions[selected];
+                                        doSystemUIReboot(context);
+                                        return;
+                                    } else if (selected >= 0 && selected < actions.length) {
                                         mReason = actions[selected];
                                         if (actions[selected].equals(SOFT_REBOOT)) {
                                             doSoftReboot();
@@ -311,6 +318,10 @@ public final class ShutdownThread extends Thread {
         } catch (RemoteException e) {
             Log.e(TAG, "failure trying to perform soft reboot", e);
         }
+    }
+
+    private static void doSystemUIReboot(Context context) {
+        Helpers.restartSystemUI(context);
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver
