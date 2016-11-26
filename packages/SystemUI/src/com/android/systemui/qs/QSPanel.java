407ae6d3fcbaec41ed0548372b88b9beaa265891
maxwen <max.weninger@gmail.com>
2016-11-26 20:04:30 +0100

    QSCustomizer: use custom column count
    AICPfy:
    Taken from https://gerrit.omnirom.org/#/c/20428/
    Adapted to work with our current solution
    
    Change-Id: I72083b7c36e71a1f1672c5c879ad8568fb66ce30

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -683,6 +683,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
                 configureTile(r.tile, r.tileView);
             }
         }
+        if (mCustomizePanel != null) {
+            mCustomizePanel.updateSettings();
+        }
     }
 }
 
