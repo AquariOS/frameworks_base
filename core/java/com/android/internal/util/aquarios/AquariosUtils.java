088b8658812128142c424a04eb96244f5e627ee6
maxwen <max.weninger@gmail.com>
2017-01-12 02:28:02 +0100

    Custom QS header images [1/2]
    Brought back up from MM days. Just now with fixed size (about 416x96dpi)
    
    Change-Id: Icb42295181684d093a686001ca4756d4b380261e

---
+++ core/java/com/android/internal/util/aquarios/AquariosUtils.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016 The Pure Nexus Project
+ * Copyright (C) 2016 Flash ROM
+ * Copyright (C) 2016 AquariOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.aquarios;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+
+import java.util.Locale;
+
+public class AquariosUtils {
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+
+}
