8be516200dc597cb575216234a1fff2a8629c981
maxwen <max.weninger@gmail.com>
2017-01-13 01:56:56 +0100

    Custom QS header extensions
    - Add static image provider that allows using a specific image from a header pack

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3727,6 +3727,12 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
 
         /**
+         * Manual override picture to use
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER_IMAGE = "status_bar_custom_header_image";
+
+        /**
          * Whether to apply a shadow on top of the header image
          * value is the alpha value of the shadow image is 0 -> no shadow -> 255 black
          * @hide
