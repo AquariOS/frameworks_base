ea46227735e3bae3494c50f7262726491e0df9ae
ezio84 <brabus84@gmail.com>
2017-02-12 02:48:56 -0500

    Advanced Bluetooth, Data, Wifi Toggles [1/2]
    Squash of:
    *Data tile behavior: make it customizable by the user -ezio84
    *QS: WiFi easy toggle [2/2] -mydogstiny
    *QS: BT easy toggle [2/2] -xyyx
    *All 3 if enabled longpress for detail view
    *Tiles easy toggles: one setting for all -nychitman1
    
    Authorship is ezios as he started this craze... :P

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7168,6 +7168,12 @@ public final class Settings {
         public static final String ADB_PORT = "adb_port";
 
         /**
+         * Whether to use one tap QS tile action to enable or disable mobile data, wifi, and bluetooth
+         * @hide
+         */
+        public static final String QS_EASY_TOGGLE = "qs_easy_toggle";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
