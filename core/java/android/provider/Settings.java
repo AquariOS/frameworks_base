92f7155b18bb88a15821588ecdd7970648439302
Danesh M <daneshm90@gmail.com>
2017-02-19 00:08:01 +0100

    base: SystemUI: [QS] Add adb over network tile
    original from Danesh
    
    adapted to 7.1 by darkobas
    
    Change-Id: I970b51ff76bbb668a346402c7fdaf314672be230
    
    QS: fix adbovernetwork tile icon visibility
    
    Change-Id: I2de466dec4928d0cbdd1ebc157d1f480adde3cb8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13227,6 +13227,12 @@ public final class Settings {
         public static final String ALLOW_SIGNATURE_FAKE = "allow_signature_fake";
 
         /**
+         * The TCP/IP port to run ADB on, or 0 for USB
+         * @hide
+         */
+        public static final String WIRELESS_OVER_ADB = "wireless_over_adb";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
