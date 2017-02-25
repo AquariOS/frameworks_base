753feb438ad4bfc797dd10f1fb43872c4c5c3b16
maxwen <max.weninger@gmail.com>
2017-02-25 17:31:05 +0100

    [1/2] base: SystemUI: OmniJaws qs tile integration
    Change-Id: I4a00255a362298820f9f7997b3089e135358e0f6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4324,6 +4324,11 @@ public final class Settings {
         public static final String DOZE_SCREEN_BRIGHTNESS = "doze_screen_brightness";
 
         /**
+         * @hide
+         */
+        public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -58,6 +58,7 @@ import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
+import com.android.systemui.qs.tiles.WeatherTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.statusbar.policy.BatteryController;
@@ -439,6 +440,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(this);
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(this);        
         else if (tileSpec.equals("reboot")) return new RebootTile(this);
+        else if (tileSpec.equals("weather")) return new WeatherTile(this);
 
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
@@ -456,13 +458,6 @@ public class QSTileHost implements QSTile.Host, Tunable {
             tileList = res.getString(R.string.quick_settings_tiles);
             if (DEBUG) Log.d(TAG, "Loaded tile specs from config: " + tileList);
         } else {
-            for (String tile : res.getString(R.string.quick_settings_tiles).split(",")) {
-                tile = tile.trim();
-                if (tile.isEmpty()) continue;
-                if(tile.startsWith(IntentTile.PREFIX)){
-                    tileList = tileList.concat(",").concat(tile);
-                }
-            }
             if (DEBUG) Log.d(TAG, "Loaded tile specs from setting: " + tileList);
         }
         final ArrayList<String> tiles = new ArrayList<String>();
