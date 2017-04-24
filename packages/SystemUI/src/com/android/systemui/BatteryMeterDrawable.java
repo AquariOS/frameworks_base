9bc5e37b37e7ccb386259dba7ec1eec4748f73db
rc420head <dennislebhardt@yahoo.com>
2017-04-24 17:57:57 -0500

    merge dingleball left hanging

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -106,17 +106,6 @@ public class BatteryMeterDrawable extends Drawable implements
     private boolean mForceChargeBatteryText;
     private int  mTextChargingSymbol;
     private boolean mListening;
-    private static final int ADD_LEVEL = 10;
-    private static final int ANIM_DURATION = 500;
-    private int mAnimOffset;
-    private boolean mCharging;
-
-    private final Runnable mInvalidate = new Runnable() {
-        @Override
-        public void run() {
-            invalidateSelf();
-        }
-    };
 
     private float mTextX, mTextY; // precalculated position for drawText() to appear centered
 
@@ -256,7 +245,12 @@ public class BatteryMeterDrawable extends Drawable implements
     }
 
     private void postInvalidate() {
-        mHandler.post(mInvalidate);
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                invalidateSelf();
+            }
+        });
     }
 
     public void setBatteryController(BatteryController batteryController) {
@@ -268,7 +262,6 @@ public class BatteryMeterDrawable extends Drawable implements
     public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
         mLevel = level;
         mPluggedIn = pluggedIn;
-        mCharging = charging;
         if (level < 31) {
             isPctToBeWhiteOrRed = true;
         }
@@ -284,26 +277,6 @@ public class BatteryMeterDrawable extends Drawable implements
     public void onPowerSaveChanged(boolean isPowerSave) {
         mPowerSaveEnabled = isPowerSave;
         invalidateSelf();
-    }
-
-    private int updateChargingAnimLevel() {
-        int curLevel = mLevel;
-        if (!mCharging) {
-            mAnimOffset = 0;
-            mHandler.removeCallbacks(mInvalidate);
-        } else {
-            curLevel += mAnimOffset;
-            if (curLevel >= FULL) {
-                curLevel = 100;
-                mAnimOffset = 0;
-            } else {
-                mAnimOffset += ADD_LEVEL;
-            }
-
-            mHandler.removeCallbacks(mInvalidate);
-            mHandler.postDelayed(mInvalidate, ANIM_DURATION);
-        }
-        return curLevel;
     }
 
     @Override
@@ -460,11 +433,6 @@ public class BatteryMeterDrawable extends Drawable implements
 
     @Override
     public void draw(Canvas c) {
-        final boolean showChargingAnim
-                = mContext.getResources().getBoolean(R.bool.config_show_battery_charging_anim);
-        final int level = showChargingAnim
-                ? updateChargingAnimLevel()
-                : mLevel;
         if (!mInitialized) {
             init();
         }
