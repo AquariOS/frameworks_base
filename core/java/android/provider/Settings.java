1f524d44dc1269f2f8956a4ead1a25895e16cce5
beanstown106 <nbenis106@gmail.com>
2017-06-02 20:41:00 -0400

    Show seconds clock (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4205,6 +4205,12 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
 
         /**
+         * Display second in the Clock
+         * @hide
+         */
+        public static final String CLOCK_USE_SECOND = "clock_use_second";
+
+        /**
          * Change fonts for the system lockscreen clock widget
          * @hide
          */
