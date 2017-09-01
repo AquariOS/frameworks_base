0b9cc2576533e7772ec6ef11ae1347ff67f3b22c
maxwen <max.weninger@gmail.com>
2017-09-01 15:54:53 +0200

    [1/2] base: allow to swap volume buttons rotation based
    abc edit: enable by def
    Change-Id: Ia50d9d1d8a72a649dd86b99f9701b0de05252ebe
    
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4053,6 +4053,12 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_BAR_BLEND_COLORS_REVERSE = "battery_bar_blend_color_reverse";
 
         /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
