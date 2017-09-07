b67109cfe3ca32018440d80f307fb9273e8b9c19
xyyx <xyyx@mail.ru>
2017-09-07 21:14:50 +0800

    Lockscreen Weather: OmniJaws version
    Reworked by @xyyx
    Based on:
    Keyguard: Create lockscreen weather by Decad3nce
    Lock screen: Weather panel improvements by XXMrHyde
    make lockscreen weather fonts a bit smaller by darkobas
    Fix NPE with WeatherStatus on KeyguardStatusView by tobitege
    Fix weather items npe by beanstown106
    Use Omnijaws API thanks to omni rom and Max Winger by varund7726
    Fix Weather on Lockscreen not updating by varund7726
    Extend OmniJaws Client to Keyguard by maxwen
    base: SystemUI: use new OmniJaws error broadcasts
    base: SystemUI: remove force weather update bits
    KeyguardWeather: Update weather after switching options by xyyx
    
    I'm sorry if I missed someone or something
    
    Change-Id: I2a714bfcbcc855ef5dd063092c60891430eeaa64
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4504,6 +4504,23 @@ public final class Settings {
         public static final String STATUS_BAR_WEATHER_IMAGE_COLOR = "status_bar_weather_image_color";
 
         /**
+         * Whether to show the weather info on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER = "lock_screen_show_weather";
+
+        /**
+         * Whether to show the weather location lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER_LOCATION = "lock_screen_show_weather_location";
+
+        /**
+         * @hide
+         */
+        public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
