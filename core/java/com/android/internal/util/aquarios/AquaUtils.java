7a1ee2b26a9a7e53d63af7dae404d68a57965a5d
bigrushdog <randall.rushing@gmail.com>
2017-09-20 21:40:59 -0700

    DUI: Initial checkin for Oreo [5/7]
    Squashed commit of the following:
    
    Manually bring in
    
    DUI: Migrate to API/Permission model for Editor [1/5]
    https://github.com/bigrushdog/android_frameworks_base/commit/20681de6a18ef2e1851d66c095658a4f1479c054
    
    commit d3dd2b5df493989f3bbb479a5734df3a84d9598a
    Author: bigrushdog <randall.rushing@gmail.com>
    Date:   Fri Aug 11 18:15:22 2017 -0700
    
        DUtils: Migrate from broadcasts to API for some action handling [1/2]
    
        Protected broadcasts for ActionHandler actions will become problematic
        in the future. Currently, broadcasts are sent from SystemUI as well as
        system server (hwkeys). This may also change in the future. So provide
        a general API solution with properly secured public methods.
    
        Change-Id: I90ee0dd5fe75545d50ca77dd0fecba86ab23a09e
    
    commit 6a528e1261649a404ee70ea74dda1551f53a30e1
    Author: bigrushdog <randall.rushing@gmail.com>
    Date:   Wed Aug 2 16:10:54 2017 -0700
    
        DUI: Left in landscape navbar API [1/3]
    
        Window manager will let SystemUI know when navbar
        enters or exits the "left in landscape" position.
        We tried to avoid this using orientation listeners
        but that approach proved unreliable leaving Pulse
        in bizarre states when navbar was left in landscape
    
        Change-Id: Ie67b905755cd20219f08e76232a10fde50ec9b85
    
    commit 7e8cb8b0cfa47d0b1e1105f97905167efafac851
    Author: Randall Rushing <randall.rushing@gmail.com>
    Date:   Thu Oct 29 05:02:04 2015 -0700
    
        Pulse: forward port visualizer lock api
    
        Change-Id: I13c7bbc8f4dc79fa41f11cbab25c1cdd83aed2af
    
    commit 593d60fcea0a2edfebb1ed9ebc21ab69238cb7e7
    Author: Randall Rushing <randall.rushing@gmail.com>
    Date:   Thu Jan 7 02:41:02 2016 -0800
    
        statusbar: add screenPinningStateChanged(boolean enabled) api and pipe to navigation bar
    
        Let activity stack notify statusbar when task locking state changes. This is needed for
        alternate task managers (OmniSwitch, SlimRecents) that implement screen pinning but don't
        interact with the screen pinning pop-up. NavigationController needs to be notified of the
        event to load a secure navigation bar.
    
        Change-Id: I948667509d448bb91bea9bd560820d8bbf456f3f
    
    commit ef32637c25689fdd577a8b022991ea22a86b55c5
    Author: bigrushdog <randall.rushing@gmail.com>
    Date:   Thu Aug 25 01:44:40 2016 -0700
    
        Bring back some removed statusbar API
    
        Some of the needed statusbar API was moved to internal implementations
        for N. Expose needed methods for DUtils library
    
        Change-Id: Ie8b19b44698159e288a3ff901208a35dfac855d6
    
    Change-Id: I712e34619aa4a26622a05d37cca2cda443455ec4

--- core/java/com/android/internal/util/aquarios/AquaUtils.java
+++ core/java/com/android/internal/util/aquarios/AquaUtils.java
@@ -1,19 +1,19 @@
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
+/*
+* Copyright (C) 2017 AquariOS
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.android.internal.util.aquarios;
 
 import android.app.ActivityManager;
@@ -37,7 +37,6 @@ import android.hardware.input.InputManager;
 import android.net.ConnectivityManager;
 import android.nfc.NfcAdapter;
 import android.provider.Settings;
-import android.content.Context;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.Handler;
@@ -159,7 +158,6 @@ public class AquaUtils {
 /**
  * Some custom utilities
  */
-public class AquaUtils {
 
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
@@ -267,5 +265,4 @@ public class AquaUtils {
             return false;
         }
     }
-  }
 }
