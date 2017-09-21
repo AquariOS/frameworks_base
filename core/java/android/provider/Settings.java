76272d048d03dd84e389fd36e918bba3b39205ec
Carsten Hauge <carsten.hauge@sonymobile.com>
2017-09-21 22:45:21 +0200

    Vibrate on plug [1/2]
    ---
    abc ezio84: remove sound part and vibrate also when normal ringer mode
    use new VibrationEffect api 26
    play vibration also on disconnected charge
    add Settings part
    get services on init
    ---
    
    original commit: https://android-review.googlesource.com/#/c/platform/frameworks/base/+/243420/
    
    Change-Id: I5d4d25861c7ddba5ce6c1cb3da223e8eaf279f8b
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4217,6 +4217,12 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
         /**
+         * Whether to vibrate on power connection or disconnection
+         * @hide
+         */
+        public static final String VIBRATION_ON_CHARGE_STATE_CHANGED = "vibration_on_charge_state_changed";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
