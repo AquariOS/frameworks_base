fd3bda38a09c282361389d35b8e115cf004faa73
maxwen <max.weninger@gmail.com>
2017-10-10 02:37:42 +0200

    [1/3] base: make outline the default weather icon pack
    Change-Id: I7205a3bb522b52098e1e386083cd8106dddddaa0
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/OmniJawsClient.java
+++ packages/SystemUI/src/com/android/keyguard/OmniJawsClient.java
@@ -49,7 +49,7 @@ public class OmniJawsClient {
             = Uri.parse("content://org.omnirom.omnijaws.provider/settings");
 
     private static final String ICON_PACKAGE_DEFAULT = "org.omnirom.omnijaws";
-    private static final String ICON_PREFIX_DEFAULT = "weather";
+    private static final String ICON_PREFIX_DEFAULT = "outline";
 
     public static final String[] WEATHER_PROJECTION = new String[]{
             "city",
