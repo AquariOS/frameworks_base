59f05bf1bed0195c87e23a6f2130112185938d36
calebcabob <calphonic@gmail.com>
2018-01-09 00:11:01 -0500

    Add Omni's PackageUtils - credit: maxwen
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/com/android/internal/util/aquarios/AquaUtils.java
+++ core/java/com/android/internal/util/aquarios/AquaUtils.java
@@ -73,6 +73,32 @@ public class AquaUtils {
         }
     }
 
+/**
+ * Omni's Pacxkage Utils
+ */
+    public static boolean isAppInstalled(Context context, String appUri) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+
     private static int getScreenType(Context context) {
         if (sDeviceType == -1) {
             WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
