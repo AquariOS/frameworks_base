35fef43b1c2a57f2cc5bdfca4cb1a3ebf41154aa
maxwen <max.weninger@gmail.com>
2018-01-22 04:12:47 +0100

    [1/2] base: custom header image provider
    reads image uri and saves it in the file
    in /data/user_de/0/com.android.systemui/files/custom_file_header_image
    the file must be copied else loading on reboot will fail cause storage
    may not be mounted
    
    Change-Id: I95f7d86f15cf00bb7dfecc3324c00f28e987d013
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4320,6 +4320,11 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER_IMAGE = "status_bar_custom_header_image";
 
         /**
+        * @hide
+        */
+       public static final String STATUS_BAR_FILE_HEADER_IMAGE = "status_bar_file_header_image";
+
+        /**
          * Boolean value whether to link ringtone and notification volume
          *
          * @hide
