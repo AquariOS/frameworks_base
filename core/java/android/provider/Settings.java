7f413c0e2f6909f31960eea74c79fa67aeec4b4e
maxwen <max.weninger@gmail.com>
2018-03-12 02:07:23 +0100

    SystemUI: minimal qs header mode [1/2]
    allow hiding the qs header icons and create smaller header
    
    BenzoEdit: Remove tuner stuff and add system setting
    Change-Id: Ie4a564965525d746d413152431fb2a4c747db792
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4535,6 +4535,11 @@ public final class Settings {
         public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
 
         /**
+         * @hide
+         */
+        public static final String QS_SHOW_MINI = "qs_show_mini";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
