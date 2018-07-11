25deb38dcd2311cd52e55e2ebd08f9ef651d56bc
calebcabob <calphonic@gmail.com>
2018-07-10 21:23:09 -0400

    Add back "Lock Screen Weather: OmniJaws version" [1/2]
    This reverts commit 369eba03abd53f1ba69c5c328d7ba71340ef20eb.
    
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4482,6 +4482,18 @@ public final class Settings {
         public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
 
         /**
+         * Whether to show the weather info on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER = "lock_screen_show_weather";
+
+        /**
+         * Whether to show the weather location lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER_LOCATION = "lock_screen_show_weather_location";
+
+        /**
          * Force expanded notifications on all apps that support it.
          * @hide
          */
