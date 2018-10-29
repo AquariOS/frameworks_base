25d8df054bcb728550f9d46568d2c62c0ddbda12
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-29 17:32:21 -0300

    SystemUI: Implement weather on lockscreen like Pixels
    This use OmniJaws implementations as base, very thanks to Omni team
    
    Icons extracted from Lawnchair, thanks to deletescape and paphonb
    
    WeatherClient is included on vendor/pixelstyle
    
    Our edits:
    
    * Cleanup code
    * Adapt to our weather provider
    * Slice-ify
    * Add required permissions
    
    Change-Id: I05c3d109edb9c38ad11e14dfc17f4dd06a9bcb1d
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/Utils.java
+++ packages/SettingsLib/src/com/android/settingslib/Utils.java
@@ -31,6 +31,7 @@ import com.android.settingslib.wrapper.LocationManagerWrapper;
 import java.text.NumberFormat;
 
 import com.android.internal.util.aquarios.ambientplay.AmbientPlayHistoryManager;
+import com.android.internal.util.aquarios.weather.WeatherClient;
 
 public class Utils {
 
@@ -289,6 +290,7 @@ public class Utils {
                 || pkg.packageName.equals(sSharedSystemSharedLibPackageName)
                 || pkg.packageName.equals(PrintManager.PRINT_SPOOLER_PACKAGE_NAME)
                 || pkg.packageName.equals(AmbientPlayHistoryManager.SERVICE_PACKAGE)
+                || pkg.packageName.equals(WeatherClient.SERVICE_PACKAGE)
                 || isDeviceProvisioningPackage(resources, pkg.packageName);
     }
 
