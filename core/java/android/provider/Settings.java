e026fb695aa866c5c264af486d0dc8c7aebc4e75
ezio84 <brabus84@gmail.com>
2018-11-01 11:20:55 +0100

    Full nav gesture: home action on the whole bar and doubletap2sleep [1/2]
    all this stuff comes from what we did on dui (waiting for it on P!)
    so huge thanks to bigrushdog
    
    Change-Id: Iad5274dea9b72da01a72418f78500f4aa0765228

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4558,6 +4558,11 @@ public final class Settings {
         public static final String FULL_GESTURE_NAVBAR = "full_gesture_navbar";
 
         /**
+         * @hide
+         */
+        public static final String FULL_GESTURE_NAVBAR_DT2S = "full_gesture_navbar_dt2s";
+
+        /**
          * Apps to skip for Pulse
          * @hide
          */
