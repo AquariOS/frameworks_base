0e66c1cf8a010fd9e5c459502bdf5190a8e38b8e
maxwen <max.weninger@gmail.com>
2018-11-05 11:08:03 -0500

    Persist screenshot type after reboot
    save state set in qs tile into settings and use that after boot
    
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4409,6 +4409,13 @@ public final class Settings {
         public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
 
         /**
+         * 0 - fullscreen
+         * 1 - partial
+         * @hide
+         */
+        public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
