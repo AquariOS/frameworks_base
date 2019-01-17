d0a61d29f0b7cea98415374fef2f489f27f615ee
bigrushdog <randall.rushing@gmail.com>
2019-01-16 21:35:17 -0800

    base: Introduce FontService In addition to shipping with a series of fonts, this service also supports user apps that provide fonts. To create a font provider app, put the following bool into your package resources
    <bool name="custom_rom_font_provider">true</bool>
    
    Then put the font zips into
    
    /res/assets/fonts/font1.zip... font2.zip...
    
    Change-Id: Ie3b4e3e740956f34b0ffb45c4632c2f62f26f48a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4514,6 +4514,20 @@ public final class Settings {
         public static final Validator STATUS_BAR_BRIGHTNESS_CONTROL_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Hold current font name and name of the originating package Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_INFO = "font_info";
+
+        /**
+         * Hold list of installed font provider packages. Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_PACKAGES = "font_packages";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
