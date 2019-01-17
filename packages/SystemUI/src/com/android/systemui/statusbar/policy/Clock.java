e93019655294090571257fb0963e768fb09b9861
Ali B <abittin@gmail.com>
2019-01-18 00:35:45 +0300

    base: move clock observer to controller
    Clock visibility toggle is moved into the controller
    and properly integrated with the clock position selection.
    
    Change-Id: I409bfa53d26d751218f07ca2f0fe3e1c8f969db8

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -16,7 +16,6 @@
 
 package com.android.systemui.statusbar.policy;
 
-import static android.provider.Settings.Secure.STATUS_BAR_CLOCK;
 import static android.provider.Settings.Secure.STATUSBAR_CLOCK_AM_PM_STYLE;
 import static android.provider.Settings.Secure.STATUSBAR_CLOCK_DATE_DISPLAY;
 import static android.provider.Settings.Secure.STATUSBAR_CLOCK_DATE_STYLE;
@@ -213,7 +212,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
             getContext().registerReceiverAsUser(mIntentReceiver, UserHandle.ALL, filter,
                     null, Dependency.get(Dependency.TIME_TICK_HANDLER));
             Dependency.get(TunerService.class).addTunable(this, STATUSBAR_CLOCK_SECONDS,
-                    STATUS_BAR_CLOCK, STATUSBAR_CLOCK_AM_PM_STYLE, STATUSBAR_CLOCK_DATE_DISPLAY,
+                    STATUSBAR_CLOCK_AM_PM_STYLE, STATUSBAR_CLOCK_DATE_DISPLAY,
                     STATUSBAR_CLOCK_DATE_STYLE, STATUSBAR_CLOCK_DATE_FORMAT, STATUSBAR_CLOCK_DATE_POSITION);
             SysUiServiceProvider.getComponent(getContext(), CommandQueue.class).addCallback(this);
             if (mShowDark) {
@@ -330,8 +329,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
                 || STATUSBAR_CLOCK_DATE_DISPLAY.equals(key)
                 || STATUSBAR_CLOCK_DATE_STYLE.equals(key)
                 || STATUSBAR_CLOCK_DATE_FORMAT.equals(key)
-                || STATUSBAR_CLOCK_DATE_POSITION.equals(key)
-                || STATUS_BAR_CLOCK.equals(key)) {
+                || STATUSBAR_CLOCK_DATE_POSITION.equals(key)) {
             updateSettings(key, newValue);
         }
     }
@@ -622,13 +620,6 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
                 }
                 mClockDatePosition = Integer.parseInt(newValue);
                 break;
-
-            case (STATUS_BAR_CLOCK):
-                if (newValue == null || mQsHeader) {
-                    newValue = "1"; // show clock
-                }
-                setClockVisibleByUser(Integer.parseInt(newValue) != 0);
-                break;
         }
 
         if (mCalendar != null) {
@@ -642,5 +633,6 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
 
     public void setQsHeader() {
         mQsHeader = true;
+        setClockVisibleByUser(Integer.parseInt("1") != 0);
     }
 }
