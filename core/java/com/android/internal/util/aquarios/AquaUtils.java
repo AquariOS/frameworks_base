78a4fd1802b3a37d912ce3a8cff78374eaf50df0
maxwen <max.weninger@gmail.com>
2019-01-24 15:32:09 +0100

    QS status bar custom header images [1/4]
    Change-Id: Ie295f7f35208545dc3146b1fdc7ad697c5d70e16

--- core/java/com/android/internal/util/aquarios/AquaUtils.java
+++ core/java/com/android/internal/util/aquarios/AquaUtils.java
@@ -196,6 +196,20 @@ public class AquaUtils {
         }
     }
 
+    // Check to see if a package is availabe
+    public static boolean isPackageAvailable(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
     public static void takeScreenrecord(int mode) {
         IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
         try {
