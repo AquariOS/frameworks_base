0190556a4bde8c62a77f866812d0b7a63de94758
bigrushdog <randall.rushing@gmail.com>
2019-03-03 18:49:41 -0800

    Introduce FontTile (WIP)
    Change-Id: I8b781d11950aaa1e01b7fe47efcaad494bfd3cf5

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -33,6 +33,7 @@ import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.FontTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
@@ -113,6 +114,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SmartNavTile(mHost);
             case "expanded_desktop":
                 return new ExpandedDesktopTile(mHost);
+            case "font":
+                return new FontTile(mHost);
         }
 
         // Intent tiles.
