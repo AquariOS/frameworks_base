crdroidandroid/android_frameworks_base

OnTheGoTile: Fix active inactive states
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/OnTheGoTile.java
@@ -26,6 +26,7 @@ import com.android.systemui.qs.tileimpl.QSTileImpl;
 
 
 import com.android.internal.util.crdroid.OnTheGoActions;
+import com.android.internal.util.crdroid.OnTheGoUtils;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 /** Quick settings tile: OnTheGo Mode **/
@@ -56,8 +57,8 @@ public class OnTheGoTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleClick() {
-        refreshState();
         toggleState();
+        refreshState();
     }
 
     @Override
@@ -76,6 +77,9 @@ public class OnTheGoTile extends QSTileImpl<BooleanState> {
                 R.string.quick_settings_onthego_label);
         state.label = mContext.getString(R.string.quick_settings_onthego_label);
         state.icon = ResourceIcon.get(R.drawable.ic_qs_onthego);
+        state.state = OnTheGoUtils.isServiceRunning(mContext,
+                "com.android.systemui.crdroid.onthego.OnTheGoService")  ?
+                Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
     }
 
     @Override

AquariOS/frameworks_base

Change-Id: Ib5c9b817b3836cdec1740500287049d71dc7053b

packages/SystemUI/src/com/android/systemui/qs/tiles/OnTheGoTile.java
@@ -26,6 +26,7 @@ import com.android.systemui.qs.tileimpl.QSTileImpl;
 
 
 import com.android.internal.util.aquarios.OnTheGoActions;
+import com.android.internal.util.aquarios.AquaUtils;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 /** Quick settings tile: OnTheGo Mode **/
@@ -56,8 +57,8 @@ public class OnTheGoTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleClick() {
-        refreshState();
         toggleState();
+        refreshState();
     }
 
     @Override
@@ -81,6 +82,9 @@ public class OnTheGoTile extends QSTileImpl<BooleanState> {
                 R.string.quick_settings_onthego_label);
         state.label = mContext.getString(R.string.quick_settings_onthego_label);
         state.icon = ResourceIcon.get(R.drawable.ic_qs_onthego);
+        state.state = AquaUtils.isServiceRunning(mContext,
+                "com.android.systemui.aquarios.onthego.OnTheGoService")  ?
+                Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
     }
 
     @Override