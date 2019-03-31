DirtyUnicorns/android_frameworks_base

Weather MCC code cleanup
We were checking MCC twice.

Change-Id: Ia5281ddd783578446843717622aedb7a2d3260d6

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -204,8 +204,9 @@ public class KeyguardSliceProvider extends SliceProvider implements
             Log.d("WeatherClient", "addWeather: Not adding because weather condition image is unknown");
             return;
         }
-        int temperature = mWeatherInfo.getTemperature(!Utils.mccCheck(getContext()));
-        String temperatureText = Utils.mccCheck(getContext()) ?
+        final boolean fahrenheit = Utils.mccCheck(getContext());
+        int temperature = mWeatherInfo.getTemperature(!fahrenheit);
+        String temperatureText = fahrenheit ?
                 Integer.toString(temperature) + "°F" :
                 Integer.toString(temperature) + "°C";
         Icon conditionIcon = Icon.createWithResource(getContext(), mWeatherInfo.getWeatherConditionImage());

AquariOS/frameworks_base

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -203,8 +203,9 @@ public class KeyguardSliceProvider extends SliceProvider implements
             Log.d("WeatherClient", "addWeather: Not adding because weather condition image is unknown");
             return;
         }
-        int temperature = mWeatherInfo.getTemperature(!AquaUtils.mccCheck(getContext()));
-        String temperatureText = AquaUtils.mccCheck(getContext()) ?
+        final boolean fahrenheit = AquaUtils.mccCheck(getContext());
+        int temperature = mWeatherInfo.getTemperature(!fahrenheit);
+        String temperatureText = fahrenheit ?
                 Integer.toString(temperature) + "°F" :
                 Integer.toString(temperature) + "°C";
         Icon conditionIcon = Icon.createWithResource(getContext(), mWeatherInfo.getWeatherConditionImage());