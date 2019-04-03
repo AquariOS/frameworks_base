029c091bfe183f4b53a44844715dde4688c936c3
Alex <du.alexcruz@gmail.com>
2019-04-03 14:50:58 -0400

    Themes: Prevent user from setting accent & background the same colors
    This was removed in the past, but shouldn't have been.
    
    Change-Id: I30c7e235979771e39e496c34885e0aa45c7f92f5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2346,6 +2346,11 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
          return ThemeAccentUtils.isUsingBlackTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
+    // Check for black and white accent overlays
+    public void unfuckBlackWhiteAccent() {
+        ThemeAccentUtils.unfuckBlackWhiteAccent(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
+    }
+
     @Nullable
     public View getAmbientIndicationContainer() {
         return mAmbientIndicationContainer;
@@ -4323,6 +4328,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         }
         if (isUsingDarkTheme() == false && isUsingBlackTheme() == false) {
             mUiOffloadThread.submit(() -> {
+            unfuckBlackWhiteAccent();
             umm.setNightMode(UiModeManager.MODE_NIGHT_NO);
             });
         }
