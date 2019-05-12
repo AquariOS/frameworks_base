87189c42f496b93ef19a3072aa3e4cd43d38076e
paphonb <paphonb@gmail.com>
2019-05-12 16:25:36 +0000

    Q Style Clock: Make it fully translatable
    Change-Id: Ied91fc8f364d4fc8ed897a3c801e3b8576e11925

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -53,7 +53,7 @@ import android.widget.TextView;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.ViewClippingUtil;
 import com.android.keyguard.clocks.CustomAnalogClock;
-import com.android.keyguard.clocks.CustomTextClock;
+import com.android.keyguard.clocks.TypographicClock;
 import com.android.systemui.Dependency;
 import com.android.systemui.Interpolators;
 import com.android.systemui.doze.DozeLog;
@@ -81,7 +81,7 @@ public class KeyguardStatusView extends GridLayout implements
     private CustomAnalogClock mAquaClockTwoView;
     private CustomAnalogClock mAquaClockThreeView;
     private CustomAnalogClock mAquaClockFourView;
-    private LinearLayout mTextClock;
+    private TypographicClock mTextClock;
 
     private TextClock mClockView;
     private View mClockSeparator;
@@ -425,6 +425,8 @@ public class KeyguardStatusView extends GridLayout implements
         } else if (mClockSelection == 8) {
             mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br>mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br>mm"));
+        } else if (mClockSelection == 9) {
+            mTextClock.onTimeChanged();
         } else {
             mClockView.setFormat12Hour("hh\nmm");
             mClockView.setFormat24Hour("kk\nmm");
@@ -762,6 +764,9 @@ public class KeyguardStatusView extends GridLayout implements
         mKeyguardSlice.setDarkAmount(mDarkAmount);
         mClockView.setTextColor(blendedTextColor);
         mClockSeparator.setBackgroundColor(blendedTextColor);
+        if (mClockSelection == 9) {
+            mTextClock.setTextColor(blendedTextColor);
+        }
         updateVisibilities();
         updateSettings();
     }
