8203c52145a1b3f4389b4864fe4830dc0104f7de
maxwen <max.weninger@gmail.com>
2019-05-19 15:21:45 -0400

    base: SystemUI: add VPN to the icon blacklist in tuner
    Change-Id: I579d1ec7a6d66ac7055cce7d7e905bae7dc5cd9a

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -119,6 +119,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
     private boolean mBlockEthernet;
     private boolean mActivityEnabled;
     private boolean mForceBlockWifi;
+    private boolean mBlockVpn;
 
     private final IconLogger mIconLogger = Dependency.get(IconLogger.class);
 
@@ -179,6 +180,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
                 boolean blockMobile = blockList.contains(SLOT_MOBILE);
                 boolean blockWifi = blockList.contains(SLOT_WIFI);
                 boolean blockEthernet = blockList.contains(SLOT_ETHERNET);
+                boolean blockVpn = blockList.contains(SLOT_VPN);
 
                 if (blockAirplane != mBlockAirplane || blockMobile != mBlockMobile
                         || blockEthernet != mBlockEthernet || blockWifi != mBlockWifi) {
@@ -189,6 +191,11 @@ public class SignalClusterView extends LinearLayout implements NetworkController
                     // Re-register to get new callbacks.
                     mNetworkController.removeCallback(this);
                     mNetworkController.addCallback(this);
+                }
+                if (blockVpn != mBlockVpn) {
+                    mBlockVpn = blockVpn;
+                    mVpnVisible = mSecurityController.isVpnEnabled() && !mBlockVpn;
+                    apply();
                 }
                 break;
         }
@@ -231,7 +238,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        mVpnVisible = mSecurityController.isVpnEnabled();
+        mVpnVisible = mSecurityController.isVpnEnabled() && !mBlockVpn;
         mVpnIconId = currentVpnIconId(mSecurityController.isVpnBranded());
 
         for (PhoneState state : mPhoneStates) {
@@ -275,7 +282,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
         post(new Runnable() {
             @Override
             public void run() {
-                mVpnVisible = mSecurityController.isVpnEnabled();
+                mVpnVisible = mSecurityController.isVpnEnabled() && !mBlockVpn;
                 mVpnIconId = currentVpnIconId(mSecurityController.isVpnBranded());
                 apply();
             }
