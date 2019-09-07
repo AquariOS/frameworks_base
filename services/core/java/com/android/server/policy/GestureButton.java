eb17746364a98e1b2159e85c617fe5140be4655c
Vachounet <vachounet@live.fr>
2019-09-07 18:26:01 -0500

    [1/2] base: implement OP like navigation gestures Squashed
    commit 2a53f77e3e57daf518acd2a3aa3b2e4200cdd05b
    Author: Marko Man <darkobas@gmail.com>
    Date:   Wed Jul 10 12:03:24 2019 +0200
    
        [1/2] Op gestures: add swipe start height setting
    
        aosip edit: remove the omni
    
        Change-Id: I9e0b9db44f402be106806b1c45a50a75432f8cf1
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    commit f940231297384ced45d7cef469ee2deb754bac2a
    Author: Stylogey <stylogey@gmail.com>
    Date:   Wed Mar 20 22:24:11 2019 +0100
    
        OP Gestures: Fix navbar detection and landscape mode
    
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
        Change-Id: Ic888171eff85a0eddf83d07528c405794d7fecc3
    
    commit 444536474c14af61e942aa809ef7eab3d31448ca
    Author: DennySPB <dennyspb@gmail.com>
    Date:   Wed Mar 6 16:38:01 2019 +0300
    
        OP gestures: fix APP switch triggering
    
        APP Switching triggers even there is just a long press in the gesture trigger zone.
        Let's check for insignificant finger movement befor trriger it
    
        Change-Id: I4e5e2753d5fe5f55b25c1d185a928f6bb4ff7844
        Signed-off-by: DennySPB <dennyspb@gmail.com>
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    commit b5d4e709882b8fee422011abe223f330df44f806
    Author: DennySPB <dennyspb@gmail.com>
    Date:   Tue Feb 12 13:18:45 2019 +0300
    
        OP like gestures: more tweaks [1/2]
    
        * add system property for bottom swipe start threshold: ro.bottom_gesture.swipe_start.threshold (default is 20)
        * customize vibration
    
        Default gesture threshold is very low and triggers only when swiping from bootom edge of screen. For more comfortable gesture using let's provide ability to set bottom swipe zone by property.
    
        Change-Id: I98463d3c237737024d6a3edfc0719128fd9bbc88
        Signed-off-by: DennySPB <dennyspb@gmail.com>
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    commit 5d022d2138c665e1d81592ce65b3c70ddc247b1e
    Author: Dil3mm4 <dil3mm4.dev@gmail.com>
    Date:   Tue Feb 5 21:22:54 2019 +0000
    
        [OP Gestures] Enable gestures on the keyguard as well
    
        Change-Id: I9965f2f465c6f2ecbf5f4c9536681948c7432302
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    commit 1c4e969e5c4a959b6ae1e51578956a9bfb54e124
    Author: maxwen <max.weninger@gmail.com>
    Date:   Fri Dec 7 20:50:38 2018 +0100
    
        base: op gesture tweaks[1/2]
    
        Change-Id: I8dc6be8a43f76eb5e306d4347d6066e339c9f020
        Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    commit 5abadec45818f38f3f8df774ccbd40a81641f3d3
    Author: Vachounet <vachounet@live.fr>
    Date:   Sat Jul 21 21:21:37 2018 +0200
    
        [1/2] base: implement OP like navigation gestures
    
        Change-Id: I68453353d9d5d6321c1a385744e5b90e2e1a413e
    
        base: add long press home handling to nav gestures
    
        insread of fixed recents use the long press home setting
    
        Change-Id: I94ef0dcb5180c1b305438acf445767fa50f6b670
    
        base: cleanup nav gestures code
    
        Change-Id: I6d412e58e2484cb84307437f67577a5c584c913b
    
        base: cleanup and fix a few nav gestures issues
    
        make sure we dont do anything while on keyguard
        long press should only be active in home location
        make threshold values resource values
        make sure if gestures are disabled no edge events are lost
    
    Change-Id: Ifb36c9b0c868257e73988f09dc11c991a0b3b9bb

---
+++ services/core/java/com/android/server/policy/GestureButton.java
@@ -0,0 +1,356 @@
+/*
+ *  Copyright (C) 2018 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.server.policy;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.hardware.input.InputManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.SystemClock;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.os.Vibrator;
+import android.os.VibrationEffect;
+import android.provider.Settings;
+import android.util.DisplayMetrics;
+import android.util.Slog;
+import android.view.HapticFeedbackConstants;
+import android.view.InputDevice;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.view.WindowManagerPolicyConstants.PointerEventListener;
+import android.view.inputmethod.InputMethodManagerInternal;
+
+import com.android.internal.R;
+import com.android.internal.util.aquarios.AquaUtils;
+import com.android.internal.util.aquarios.TaskUtils;
+import com.android.server.LocalServices;
+
+public class GestureButton implements PointerEventListener {
+    private static final String TAG = "GestureButton";
+    private static boolean DEBUG = false;
+
+    private static final int MSG_SEND_SWITCH_KEY = 5;
+    private static final int MSG_SEND_KEY = 6;
+    private static final int MSG_SEND_LONG_PRESS = 7;
+
+    private long mDownTime;
+    private float mFromX;
+    private float mFromY;
+    private float mLastX;
+    private float mLastY;
+    private int mNavigationBarPosition = 0;
+    private GestureButtonHandler mGestureButtonHandler;
+    private int mPreparedKeycode;
+    private PhoneWindowManager mPwm;
+    private int mScreenHeight = -1;
+    private int mScreenWidth = -1;
+    private boolean mSwipeStartFromEdge;
+    private int mSwipeStartThreshold;
+    private boolean mKeyEventHandled;
+    private boolean mRecentsTriggered;
+    private boolean mLongSwipePossible;
+    private int mEventDeviceId;
+    private boolean mDismissInputMethod;
+    private int mSwipeMinLength;
+    private int mMoveTolerance;
+    private int mSwipeTriggerTimeout;
+    private int mHapticDuration;
+    private Context mContext;
+
+    private OnTouchListener mTouchListener = new OnTouchListener() {
+        public boolean onTouch(View v, MotionEvent event) {
+            handleTouch(event);
+            return true;
+        }
+    };
+
+    private Vibrator vibrator;
+    final Handler vibrationHandler = new Handler();
+
+    final Runnable haptic = new Runnable() {
+        public void run() {
+            if (mHapticDuration == 0)
+                return;
+            vibrator.vibrate(VibrationEffect.createOneShot(mHapticDuration, VibrationEffect.DEFAULT_AMPLITUDE));
+       }
+    };
+
+    private class GestureButtonHandler extends Handler {
+
+        public GestureButtonHandler(Looper looper) {
+            super(looper);
+        }
+
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SEND_SWITCH_KEY:
+                    if (DEBUG) Slog.i(TAG, "MSG_SEND_SWITCH_KEY");
+                    mKeyEventHandled = true;
+                    vibrationHandler.postDelayed(haptic, 10);
+                    toggleRecentApps();
+                    break;
+                case MSG_SEND_KEY:
+                    if (DEBUG) Slog.i(TAG, "MSG_SEND_KEY " + mPreparedKeycode);
+                    mKeyEventHandled = true;
+                    vibrationHandler.postDelayed(haptic, 10);
+                    triggerGestureVirtualKeypress(mPreparedKeycode);
+                    break;
+                case MSG_SEND_LONG_PRESS:
+                    if (DEBUG) Slog.i(TAG, "MSG_SEND_LONG_PRESS");
+                    mKeyEventHandled = true;
+                    vibrationHandler.postDelayed(haptic, 10);
+                    TaskUtils.toggleLastApp(mContext, UserHandle.USER_CURRENT);
+                    break;
+            }
+        }
+    }
+
+    public GestureButton(Context context, PhoneWindowManager pwm) {
+        Slog.i(TAG, "GestureButton init");
+        mContext = context;
+        mPwm = pwm;
+        vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) context.getSystemService("window");
+        windowManager.getDefaultDisplay().getRealMetrics(displayMetrics);
+        mScreenHeight = Math.max(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        mScreenWidth = Math.min(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        mSwipeStartThreshold = context.getResources().getInteger(R.integer.nav_gesture_swipe_start);
+        mSwipeMinLength = getSwipeLengthInPixel(context.getResources().getInteger(R.integer.nav_gesture_swipe_min_length));
+        mMoveTolerance = context.getResources().getInteger(R.integer.nav_gesture_move_threshold);
+        mSwipeTriggerTimeout  = context.getResources().getInteger(R.integer.nav_gesture_swipe_timout);
+        mHapticDuration = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.BOTTOM_GESTURE_FEEDBACK_DURATION, 50,
+                UserHandle.USER_CURRENT);
+        HandlerThread gestureButtonThread = new HandlerThread("GestureButtonThread", -8);
+        gestureButtonThread.start();
+        mGestureButtonHandler = new GestureButtonHandler(gestureButtonThread.getLooper());
+    }
+
+    private void dismissInputMethod() {
+        mDismissInputMethod = true;
+        if (mPwm.mInputMethodManagerInternal == null) {
+            mPwm.mInputMethodManagerInternal = (InputMethodManagerInternal) LocalServices.getService(InputMethodManagerInternal.class);
+        }
+        if (mPwm.mInputMethodManagerInternal != null) {
+            mPwm.mInputMethodManagerInternal.hideCurrentInputMethod();
+        }
+    }
+
+    public void onPointerEvent(MotionEvent event) {
+        handleTouch(event);
+    }
+
+    private void handleTouch(MotionEvent event) {
+        int action = event.getActionMasked();
+        mEventDeviceId = event.getDeviceId();
+
+        if (action == MotionEvent.ACTION_DOWN || mSwipeStartFromEdge) {
+            float rawX = event.getRawX();
+            float rawY = event.getRawY();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    mPreparedKeycode = -1;
+                    if (mNavigationBarPosition == 0) {
+                        if (rawY >= ((float) (mScreenHeight - mSwipeStartThreshold))) {
+                            mFromX = rawX;
+                            mFromY = rawY;
+                            if (mFromX < ((float) (mScreenWidth / 3)) || mFromX > ((float) ((mScreenWidth * 2) / 3))) {
+                                mPreparedKeycode = KeyEvent.KEYCODE_BACK;
+                            } else {
+                                mPreparedKeycode = KeyEvent.KEYCODE_HOME;
+                            }
+                        }
+                    } else if ((mNavigationBarPosition != 1 || rawX >= ((float) (mScreenHeight - mSwipeStartThreshold))) && (mNavigationBarPosition != 2 || rawX <= ((float) mSwipeStartThreshold))) {
+                        mFromX = rawX;
+                        mFromY = rawY;
+                        if (mFromY < ((float) (mScreenWidth / 3)) || mFromY > ((float) ((mScreenWidth * 2) / 3))) {
+                            mPreparedKeycode = KeyEvent.KEYCODE_BACK;
+                        } else {
+                            mPreparedKeycode = KeyEvent.KEYCODE_HOME;
+                        }
+                    }
+                    if (mPreparedKeycode != -1) {
+                        mLastY = mFromY;
+                        mLastX = mFromX;
+                        mDownTime = event.getEventTime();
+                        mSwipeStartFromEdge = true;
+                        mKeyEventHandled = false;
+                        mRecentsTriggered = false;
+                        mLongSwipePossible = false;
+                        if (DEBUG) Slog.i(TAG, "ACTION_DOWN " + mPreparedKeycode + " mMoveTolerance = " + mMoveTolerance);
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if (mPreparedKeycode != -1) {
+                        if (DEBUG)
+                            Slog.i(TAG, "ACTION_UP " + mPreparedKeycode + " " + mRecentsTriggered + " " + mKeyEventHandled + " " + mLongSwipePossible);
+                        mGestureButtonHandler.removeMessages(MSG_SEND_SWITCH_KEY);
+                        mGestureButtonHandler.removeMessages(MSG_SEND_LONG_PRESS);
+                        cancelPreloadRecentApps();
+
+                        if (!mKeyEventHandled && mLongSwipePossible) {
+                            if (mPreparedKeycode == KeyEvent.KEYCODE_HOME) {
+                                if (!mDismissInputMethod) {
+                                    dismissInputMethod();
+                                }
+                            }
+                            mGestureButtonHandler.sendEmptyMessage(MSG_SEND_KEY);
+                        }
+                        mSwipeStartFromEdge = false;
+                        mKeyEventHandled = false;
+                        mRecentsTriggered = false;
+                        mLongSwipePossible = false;
+                    }
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if (!mKeyEventHandled && !mRecentsTriggered && mPreparedKeycode != -1) {
+                        float moveDistanceSinceDown;
+                        if (mNavigationBarPosition == 0) {
+                            moveDistanceSinceDown = Math.abs(mFromY - rawY);
+                        } else {
+                            moveDistanceSinceDown = Math.abs(mFromX - rawX);
+                        }
+                        float moveDistanceSinceLast;
+                        if (mNavigationBarPosition == 0) {
+                            moveDistanceSinceLast = Math.abs(mLastY - rawY);
+                        } else {
+                            moveDistanceSinceLast = Math.abs(mLastX - rawX);
+                        }
+                        long deltaSinceDown = event.getEventTime() - mDownTime;
+                        if (mPreparedKeycode == KeyEvent.KEYCODE_HOME && moveDistanceSinceDown < mSwipeMinLength) {
+                            if (( moveDistanceSinceDown > 20) && (moveDistanceSinceLast < mMoveTolerance)) {
+                                if (DEBUG) Slog.i(TAG, "long click: moveDistanceSinceLast = " + moveDistanceSinceLast);
+                                mGestureButtonHandler.removeMessages(MSG_SEND_LONG_PRESS);
+                                mGestureButtonHandler.sendEmptyMessageDelayed(MSG_SEND_LONG_PRESS, mSwipeTriggerTimeout);
+                            } else {
+                                mGestureButtonHandler.removeMessages(MSG_SEND_LONG_PRESS);
+                            }
+                        }
+
+                        if (moveDistanceSinceDown > mSwipeMinLength) {
+                            mGestureButtonHandler.removeMessages(MSG_SEND_LONG_PRESS);
+                            if (DEBUG) Slog.i(TAG, "swipe: moveDistanceSinceDown = " + moveDistanceSinceDown);
+                            mLongSwipePossible = true;
+                            if (mPreparedKeycode == KeyEvent.KEYCODE_BACK) {
+                                // TODO should back be triggered already while move? so without up
+                                //mGestureButtonHandler.sendEmptyMessage(MSG_SEND_KEY);
+                            } else if (!mRecentsTriggered) {
+                                // swipe comes to an stop
+                                if (moveDistanceSinceLast < mMoveTolerance) {
+                                    mRecentsTriggered = true;
+                                    preloadRecentApps();
+                                    mGestureButtonHandler.removeMessages(MSG_SEND_SWITCH_KEY);
+                                    mGestureButtonHandler.sendEmptyMessageDelayed(MSG_SEND_SWITCH_KEY, mSwipeTriggerTimeout);
+                                }
+                            }
+                        }
+                        mLastX = rawX;
+                        mLastY = rawY;
+                    }
+                    break;
+                case MotionEvent.ACTION_CANCEL:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void toggleRecentApps() {
+        mPwm.toggleRecentApps();
+    }
+
+    private void preloadRecentApps() {
+        mPwm.preloadRecentApps();
+    }
+
+    private void cancelPreloadRecentApps() {
+        mPwm.cancelPreloadRecentApps();
+    }
+
+    private void triggerGestureVirtualKeypress(int keyCode) {
+        AquaUtils.sendKeycode(keyCode);
+    }
+
+    void navigationBarPosition(int displayWidth, int displayHeight, int displayRotation) {
+        int navigationBarPosition = 0;
+        if (displayWidth > displayHeight) {
+            if (displayRotation == 3) {
+                navigationBarPosition = 2;
+            } else {
+                navigationBarPosition = 1;
+            }
+        }
+        if (navigationBarPosition != mNavigationBarPosition) {
+            mNavigationBarPosition = navigationBarPosition;
+        }
+    }
+
+    boolean isGestureButtonRegion(int x, int y) {
+        boolean isregion = true;
+        if (mNavigationBarPosition == 0) {
+            if (y < mScreenHeight - mSwipeStartThreshold) {
+                isregion = false;
+            }
+        } else if (mNavigationBarPosition == 1) {
+            if (x < mScreenHeight - mSwipeStartThreshold) {
+                isregion = false;
+            }
+        } else {
+            if (x > mSwipeStartThreshold) {
+                isregion = false;
+            }
+        }
+        return isregion;
+    }
+
+    void updateSettings() {
+        mSwipeTriggerTimeout = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT,
+                mContext.getResources().getInteger(R.integer.nav_gesture_swipe_timout),
+                UserHandle.USER_CURRENT);
+        mSwipeMinLength = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT,
+                getSwipeLengthInPixel(mContext.getResources().getInteger(R.integer.nav_gesture_swipe_min_length)),
+                UserHandle.USER_CURRENT);
+        mHapticDuration = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.BOTTOM_GESTURE_FEEDBACK_DURATION, 50,
+                UserHandle.USER_CURRENT);
+        mSwipeStartThreshold = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.BOTTOM_GESTURE_SWIPE_START,
+                getSwipeLengthInPixel(mContext.getResources().getInteger(R.integer.nav_gesture_swipe_start)),
+                UserHandle.USER_CURRENT);
+
+        if (DEBUG) Slog.i(TAG, "updateSettings mSwipeTriggerTimeout = " + mSwipeTriggerTimeout + " mSwipeMinLength = " + mSwipeMinLength);
+    }
+
+    private int getSwipeLengthInPixel(int value) {
+        return Math.round(value * mContext.getResources().getDisplayMetrics().density);
+    }
+}
