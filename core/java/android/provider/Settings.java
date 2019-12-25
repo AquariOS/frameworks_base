3c4dfb805fd6b869f79ad44d3d0b345fd8882d32
AnkitGourav <Ankitgourav035@gmail.com>
2019-12-25 11:10:02 +0530

    Disable Notifications vibration [1/2]
    Change-Id: I14cbba672554d8cdc3b486d6ddaca6b5289486f4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5042,6 +5042,12 @@ public final class Settings {
 
 
         /**
+         * Whether to vibrate on notifications
+         * @hide
+         */
+        public static final String VIBRATE_ON_NOTIFICATIONS = "vibrate_on_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
