63780c7bbb8f46e1e29935c3f7d1e21af9fe7a81
ezio84 <brabus84@gmail.com>
2019-12-26 22:18:53 +0100

    Ambient music: send the proper pulse reason on slpi tap
    fox: add missed part of commit 8a5ab67b99b329b3142709fe2f4aecbe60435eed
    
    Change-Id: I18e77acb06c925153d45a6c04ac5bfd0e16f5ad8
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -186,9 +186,9 @@ public class DozeTriggers implements DozeMachine.Part {
                 }
                 if (isDoubleTap || isTap) {
                     if (screenX != -1 && screenY != -1) {
-                        mDozeHost.onSlpiTap(screenX, screenY);
+                        mDozeHost.onSlpiTap(screenX, screenY, pulseReason);
                     }
-                    gentleWakeUp(pulseReason);
+                    //gentleWakeUp(pulseReason);
                 } else if (isPickup) {
                     gentleWakeUp(pulseReason);
                 } else {
@@ -582,6 +582,11 @@ public class DozeTriggers implements DozeMachine.Part {
             if (mDozeHost.isPowerSaveActive()) {
                 mMachine.requestState(DozeMachine.State.DOZE);
             }
+        }
+
+        @Override
+        public void wakeUpFromDoubleTap(int pulseReason) {
+            gentleWakeUp(pulseReason);
         }
     };
 }
