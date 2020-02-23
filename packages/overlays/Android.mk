80b3a3916bb24ae4ee7ef72d8a53d7499c65139e
calebcabob <calphonic@gmail.com>
2020-02-23 08:54:08 -0500

    themes: Add hexagon shape overlay package
    Change-Id: Iaf45c9537e1943cb800311510ca55b6ea4d3de32
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -48,6 +48,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconShapeSquircleOverlay \
 	IconShapeTeardropOverlay \
 	IconShapeCircleOverlay \
+	IconShapeHexagonOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
